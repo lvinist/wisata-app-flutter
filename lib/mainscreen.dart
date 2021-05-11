03a77cda60f0febc506ae99b477302375089c65d
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-05-11 23:57:25 +0700

    implement nav to detail screen

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,8 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:wisata_app/detail_screen.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Wisata di Bandung'),
+        ),
+        body: InkWell(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return DetailScreen();
+            }));
+          },
+          child: Card(
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Image.asset('images/bosscha.jpg'),
+                ),
+                Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            'Wisata Bosscha',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Lembang',
+                            style: TextStyle(fontSize: 12),
+                          )
+                        ],
+                      ),
+                    ))
+              ],
+            ),
+          ),
+        ));
   }
 }
