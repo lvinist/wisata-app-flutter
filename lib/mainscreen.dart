ca1526372b474cb2a8614480edcc114c10b66078
Muhammad Alvin Ali Fikri <alvin.geomatics@gmail.com>
2021-05-12 02:43:59 +0700

    implement grid view on larger screen

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -6,9 +6,29 @@ class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Wisata di Bandung'),
-      ),
+        appBar: AppBar(
+          title: Text('Wisata di Bandung'),
+        ),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth <= 700) {
+              return TourismListView();
+            } else if (constraints.maxWidth <= 900) {
+              return TourismGridView(gridCount: 4);
+            } else if (constraints.maxWidth <= 1200) {
+              return TourismGridView(gridCount: 5);
+            } else {
+              return TourismGridView(gridCount: 6);
+            }
+          },
+        ));
+  }
+}
+
+class TourismListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: ListView.builder(
         itemBuilder: (context, index) {
           final TourismPlace place = tourismPlaceList[index];
@@ -56,6 +76,65 @@ class MainScreen extends StatelessWidget {
           );
         },
         itemCount: tourismPlaceList.length,
+      ),
+    );
+  }
+}
+
+class TourismGridView extends StatelessWidget {
+  final int gridCount;
+
+  TourismGridView({required this.gridCount});
+  @override
+  Widget build(BuildContext context) {
+    return Scrollbar(
+      isAlwaysShown: true,
+      child: Padding(
+        padding: EdgeInsets.all(24),
+        child: GridView.count(
+          crossAxisCount: gridCount,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: tourismPlaceList.map((place) {
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return DetailScreen(place: place);
+                }));
+              },
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                        child: Hero(
+                      tag: place.imageAsset,
+                      child: Image.asset(
+                        place.imageAsset,
+                        fit: BoxFit.cover,
+                      ),
+                    )),
+                    SizedBox(height: 8),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Text(
+                        place.name,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 8, bottom: 8),
+                      child: Text(
+                        place.location,
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
